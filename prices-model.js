1ec812dbc19299f58aab6e3d5148146cb65548cf
Matt Dowds <matthewd99@gmail.com>
2016-11-26 23:08:09 +0000

    Reworked unit tests, added PricesModel functionality

--- prices-model.js
+++ prices-model.js
@@ -1,7 +1,38 @@
-var PricesModel = {
-    getData: function () {
-        
-    }
+var PricesModel = function(outcode, areaName, averagePrice, detachedAverage, flatAverage, semiDetachedAverage, terracedAverage, transactionCount) {
+    this.outcode = outcode;
+    this.areaName = areaName;
+    this.averagePrice = averagePrice;
+    this.detachedAverage = detachedAverage;
+    this.flatAverage = flatAverage;
+    this.semiDetachedAverage = semiDetachedAverage;
+    this.terracedAverage = terracedAverage;
+    this.transactionCount = transactionCount;
 };
 
-module.exports = PricesModel
+function getData(outcode, callback) {
+
+    var url = "/prices/" + outcode;
+
+    $.get(url)
+        .done(function(response) {
+
+            var data = JSON.parse(response);
+            var model = new PricesModel(
+                data.outcode,
+                data.areaName,
+                data.averagePrice,
+                data.detachedAverage,
+                data.flatAverage,
+                data.semiDetachedAverage,
+                data.terracedAverage,
+                data.transactionCount
+            );
+
+            callback(model, url);
+        })
+        .fail(function(data){
+            if(data.responseText == null)
+            var error = JSON.parse(data.responseText).error;
+            callback(null, url, error);
+        });
+}
--- tests/prices-model-tests.js
+++ tests/prices-model-tests.js
@@ -1,48 +1,125 @@
 'use strict';
 var assert = require('chai').assert;
-var sinon = require('sinon')
+// var sinon = require('sinon');
+var atomus = require('atomus');
+
+var xhr;
 
 describe('PricesModel', function() {
 
+    var browser;
+
+    var mockups = [
+        {
+            url: '/prices/E17',
+            response: {
+                status: 200,
+                responseText: JSON.stringify({
+                    "areaName": "London",
+                    "averagePrice": 100,
+                    "detachedAverage": 101,
+                    "flatAverage": 102,
+                    "outcode": "E17",
+                    "pastAveragePrice": 0,
+                    "priceChange": 0,
+                    "semiDetachedAverage": 103,
+                    "terracedAverage": 104,
+                    "transactionCount": 105
+                })
+            }
+        },
+        {
+            url: '/prices/invalid',
+            response: {
+                status: 500,
+                responseText: JSON.stringify({ "error": "An error occured: no valid data returned"})
+            }
+        },
+        {
+            url: '/prices/empty',
+            response: null
+        }
+    ];
+
     beforeEach(function() {
-        //var PricesModel = require('../prices-model.js');
-        //this.model = PricesModel;
-        var t = require('../prices-model.js');
-        this.model = new PricesModel()
-
-        this.ajaxRequest = sinon.useFakeXMLHttpRequest();
-
-        this.requests = [];
-        this.ajaxRequest.onCreate = function(xhr) {
-            this.requests.push(xhr);
-        }.bind(this);
+        browser = atomus()
+            .external('prices-model.js');
     });
 
     afterEach(function() {
-        this.ajaxRequest.restore();
+
     });
 
     it('should exist', function() {
-        var PricesModel = require('../prices-model.js');
-
-        assert.isNotNull(this.model)
+        browser.ready(function(errors, window) {
+            var model = new window.PricesModel();
+            assert.isDefined(model);
+        });
     });
 
     describe('#getData()', function () {
 
-        it('should parse fetched data as JSON', function(done) {
-            var data = { foo: 'bar' };
-            var dataJson = JSON.stringify(data);
+        it('should make AJAX request', function (done) {
 
-            this.model.getData(function(err, result) {
-                assert.deepEqual(result, data)
-                done();
+            var requestMade = false;
+
+            browser.ready(function (errors, window) {
+
+                browser.addXHRMock(mockups);
+
+                window.getData("E17", function () {
+                    requestMade = true;
+                    assert.isTrue(requestMade);
+                    done();
+                });
             });
+        });
 
-            // Mocks the server response: response-type, content-type, result data
-            this.requests[0].respond(200, { 'Content-Type': 'text/json' }, dataJson);
+        it('should call the correct URL', function (done) {
+
+            browser.ready(function (errors, window) {
+
+                browser.addXHRMock(mockups);
+
+                window.getData("E17", function (result, url) {
+                    assert.equal(url, "/prices/E17");
+                    done();
+                });
+            });
+        });
+
+        it('should return the correct values', function (done) {
+
+            browser.ready(function (errors, window) {
+
+                browser.addXHRMock(mockups);
+
+                window.getData("E17", function (model, url) {
+                    assert.equal(model.outcode, "E17");
+                    assert.equal(model.areaName, "London");
+                    assert.equal(model.averagePrice, 100);
+                    assert.equal(model.detachedAverage, 101);
+                    assert.equal(model.flatAverage, 102);
+                    assert.equal(model.semiDetachedAverage, 103);
+                    assert.equal(model.terracedAverage, 104);
+                    assert.equal(model.transactionCount, 105);
+
+                    done();
+                });
+            });
+        });
+
+        it('should return an error with an invalid request', function (done) {
+
+            browser.ready(function (errors, window) {
+
+                browser.addXHRMock(mockups);
+
+                window.getData("invalid", function (model, url, error) {
+                    assert.isDefined(error);
+                    done();
+                });
+            });
         });
     });
-
-
 });
